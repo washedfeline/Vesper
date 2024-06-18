22ab8a894174f237b8111c62ad839eaeb805d5c2
WashedFeline <washedfeline@gmail.com>
2024-06-18 13:07:12 -0300

    Initial commit

---
+++ src/main/java/com/washedfeline/vesper/VesperApplication.java
@@ -0,0 +1,13 @@
+package com.washedfeline.vesper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VesperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/washedfeline/vesper/VesperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.washedfeline.vesper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
